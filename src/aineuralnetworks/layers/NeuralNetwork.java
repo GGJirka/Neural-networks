ea7bd0f8d78780fa64b3925505993ef1c8344582
GGJirka <jirkazboril@seznam.cz>
2016-12-06 19:34:21 +0100

    neural network update

--- src/aineuralnetworks/layers/NeuralNetwork.java
+++ src/aineuralnetworks/layers/NeuralNetwork.java
@@ -31,11 +31,13 @@ public class NeuralNetwork {
     public LinkedList<AHiddenLayer> outputLayer;
     public double[] inputs = new double[3];
     public double[][] hiddenWeights = new double[4][3];
-    public double[] hiddenWeights2 = new double[4];
+    public double[][] hiddenWeights2 = new double[4][4];
     public double[] outputWeights = new double[4];
     public double[] surfaces = new double[4];
-    public final double JUMPING_TARGET = 0.7;
-    public final double LEARNING_RATE = 0.5;
+    public double JUMPING_TARGET = 0.5;
+    public final double DOWN_TARGET = 0.5;
+    public final double LEARNING_RATE = 0.00005;
+    public boolean isLearning = false;
     public double deltaSecond = 0;
     public double deltaFirst = 0;
     public double output = 0;
@@ -47,8 +49,8 @@ public class NeuralNetwork {
     }
 
     /*
-        Initializing weights in each layer
-     */
+    Initializing weights in each layer
+    */
     public void initializeHiddenLayer() {
         for (int l = 0; l < 4; l++) {
             for (int i = 0; i < hiddenWeights[0].length; i++) {
@@ -58,10 +60,11 @@ public class NeuralNetwork {
         }
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < hiddenWeights2.length; j++) {
-                hiddenWeights2[j] = Math.random();
+                hiddenWeights2[i][j] = Math.random();
             }
-            this.hiddenLayer2.add(new HiddenLayer(inputs, hiddenWeights2));
+            this.hiddenLayer2.add(new HiddenLayer(inputs, hiddenWeights2[i]));
         }
+        
         for (int i = 0; i < outputWeights.length; i++) {
             outputWeights[i] = Math.random();
         }
@@ -81,12 +84,14 @@ public class NeuralNetwork {
         calculating in backpropagation new weights
      */
     public void calculateHiddenLayer() {
-        double[] layer2inputs = new double[this.hiddenWeights2.length];
+        double[] layer2inputs = new double[4];
         double[] outputInputs = new double[this.hiddenWeights2.length];
+        
         /*
         *   calculating variables for first hidden layer
-         */
-        for (int i = 0; i < this.hiddenLayer.size(); i++) {
+        */
+        
+        for (int i = 0; i < this.hiddenLayer.size(); i++){
             AHiddenLayer layer = this.hiddenLayer.get(i);
             layer.inputs = this.inputs;
             layer.activationFunction(layer.calculateWeightedSum());
@@ -97,7 +102,7 @@ public class NeuralNetwork {
             }
             System.out.println("FIRST HIDDEN LAYER WEIGHTED SUM "+i+": "+layer.calculateWeightedSum());
             System.out.println("FIRST HIDDEN LAYER ACT FUNC "+i+": "+layer.activationFunction(layer.calculateWeightedSum()));
-             */
+            */
         }
 
         /*
@@ -118,16 +123,22 @@ public class NeuralNetwork {
             AHiddenLayer outputLayer = this.outputLayer.get(i);
             outputLayer.inputs = outputInputs;
             output = outputLayer.activationFunction(outputLayer.calculateWeightedSum());
+            //System.out.println(output);
 //            System.out.println("OUTPUT LAYER "+i+": "
 //                    +outputLayer.activationFunction(outputLayer.calculateWeightedSum()));
+        }
+        if(inputs[1]<=200){
+            backPropagation();
         }
     }
 
     /*
         calculating new weights for each layer
      */
-    public void adjustWeight() {
-
+    public void backPropagation() {
+        backPropagationOutput(outputLayer);
+        backPropagationSecondHidden(hiddenLayer2);
+        backPropagationFirstHidden(hiddenLayer);
     }
 
     /*
